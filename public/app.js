2188293e5ad2e542f0d38f06da846451a7ffafcb
Farheen <farheenzm@gmail.com>
2018-08-02 00:32:44 +0100

    CSS & extension of ingredients list added

--- public/app.js
+++ public/app.js
@@ -12,6 +12,7 @@ const requestComplete = function(){
   if(this.status !== 200) return;
   const jsonString = this.responseText;
   const beers = JSON.parse(jsonString);
+  const ingredients = JSON.parse(jsonString);
 
   populateList(beers);
   renderBeer(beers);
@@ -32,6 +33,7 @@ const populateList = function(beers){
 const renderBeer = function (beers) {
 
   const selectedBeer = document.querySelector('select');
+  
   selectedBeer.addEventListener('change', function(event) {
 
     let beer = beers[this.value];
@@ -42,12 +44,13 @@ const renderBeer = function (beers) {
 
 }
 
-
 const beerDetails = function(beer){
-
   const div = document.getElementById('div-beer');
   clearContent(div);
 
+  const table = document.createElement('table');
+  const tr = document.createElement('tr');
+  const td = document.createElement('td');
   const beerName = document.createElement('p');
   beerName.innerText = "Name : " + beer.name;
 
@@ -63,11 +66,54 @@ const beerDetails = function(beer){
   const beerImage = document.createElement('img');
   beerImage.src = beer.image_url;
 
+  const ingredients = document.createElement('p');
+
+  const yeastIngredient = document.createElement('p');
+  yeastIngredient.innerText = 'yeast : ' + beer.ingredients['yeast'];
+  ingredients.appendChild(yeastIngredient);
+
+  const maltIngredient = document.createElement('p');
+  maltIngredient.innerText = 'malt : \n';
+  for(var index in beer.ingredients['malt']) {
+    maltIngredient.innerText += beer.ingredients['malt'][index]['name'];
+    maltIngredient.innerText += '\n'
+  }
+  ingredients.appendChild(maltIngredient);
+
+  const hopsIngredient = document.createElement('p');
+  hopsIngredient.innerText = 'hops : \n';
+  for(var index in beer.ingredients['hops']) {
+    hopsIngredient.innerText += beer.ingredients['hops'][index]['name'];
+    hopsIngredient.innerText += '\n'
+  }
+  ingredients.appendChild(hopsIngredient);
+
+
+  // for(var key in beer.ingredients){
+  //   const ingredient = document.createElement('p');
+  //   ingredient.innerText = key + ":" + beer.ingredients[key];
+  //   const jsonString = JSON.stringify(beer);
+
+    // ingredients.innerText += "::" beer.ingredients[key][0];
+    // .forEach(function(beer, index){
+    //
+    //   let option = document.createElement('option');
+    //   option.innerText = beer.name;
+    //   option.value = index;
+    //   select.appendChild(option);
+    // });
+    //
+  //   ingredients.appendChild(ingredient);
+  // }
+
+
+
   div.appendChild(beerName);
   div.appendChild(tagline);
   div.appendChild(first_brewed);
   div.appendChild(description);
   div.appendChild(beerImage);
+  div.appendChild(ingredients);
 
   return div;
 
